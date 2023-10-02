c1acb947e913cb2cf7737d3307510e3eca7ec741
LeandroVictor666 <LeandroVictorDev@gmail.com>
2023-10-01 21:18:44 -0300

    refactor(controller/service): changed exampleFunction return type to server-response dto

--- app/api/src/register/register.controller.ts
+++ app/api/src/register/register.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { RegisterService } from './register.service';
+import { ServerResponseDto } from 'src/dto/server-response.dto';
 
 @Controller('register')
 export class RegisterController {
   constructor(private readonly registerService: RegisterService) {}
 
   @Get('example')
-  exampleFunction(): string {
+  exampleFunction(): ServerResponseDto {
     return this.registerService.exampleFunction();
   }
 }
--- app/api/src/register/register.service.ts
+++ app/api/src/register/register.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { ServerResponseDto } from 'src/dto/server-response.dto';
+import { ResponseStatus } from 'src/enum/response-status.enum';
 
 @Injectable()
 export class RegisterService {
-  exampleFunction(): string {
-    return 'It should works!';
+  exampleFunction(): ServerResponseDto {
+    return {
+      isError: false,
+      response: 'builded sucessfully!',
+      responseStatus: ResponseStatus.Success,
+      responseFrom: `server`,
+    };
   }
 }
