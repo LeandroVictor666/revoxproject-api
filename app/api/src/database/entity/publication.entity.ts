8563a825d5a91a97e360598447f53fe2579ecd84
LeandroVictor666 <LeandroVictorDev@gmail.com>
2023-10-02 00:22:37 -0300

    feat(entity): added 'publications' entity

---
+++ app/api/src/database/entity/publication.entity.ts
@@ -0,0 +1,36 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { Account } from './account.entity';
+
+@Entity()
+export class Publications {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'text', nullable: false })
+  content: string;
+
+  @Column({ type: 'int4', default: 0 })
+  likesCounter: number;
+
+  @Column({ type: 'int4', default: 0 })
+  commentsCounter: number;
+
+  @Column({ type: 'int4', default: 0 })
+  sharesCounter: number;
+
+  @ManyToOne(() => Account, (author) => author.id)
+  @JoinColumn({ name: 'authorId' })
+  author: Account;
+
+  @Column({
+    type: 'timestamp without time zone',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  publicationDate: Date;
+}
