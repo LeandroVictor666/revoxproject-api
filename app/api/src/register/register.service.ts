63164ca299524dc7ff113bae3150e2f70b5e6e7c
LeandroVictor666 <LeandroVictorDev@gmail.com>
2023-10-03 06:49:46 -0300

    feat(service): added new methods, and updated 'registerUser' method, injected ApiMessagerService
    added 2 validations methods, 'usernameExists' and 'emailExists', this 2 methods search for the
    parameter in the database, if dont exists, return false, else return a message error, this was the
    best/easiest method that i think to handle duplicated key errors (i was creating a service that
    would do several switch cases, it was becoming something huge and quite verbose), and splited
    methods responsabilities, the new 'newUser' method helps this, he is responsible to register the
    user in database, the 'registerUser' method now just call these methods, and return Promises.reject
    or Promise.resolve

--- app/api/src/register/register.service.ts
+++ app/api/src/register/register.service.ts
@@ -7,12 +7,14 @@ import { Account } from 'src/database/entity/account.entity';
 import { Repository } from 'typeorm';
 import * as BCrypt from 'bcrypt';
 import * as RegisterRules from '../rules/register-rules';
+import { ApiMessagerService } from 'src/api-messager/api-messager.service';
 
 @Injectable()
 export class RegisterService {
   constructor(
     @InjectRepository(Account)
     private accountRepository: Repository<Account>,
+    private readonly apiMessagerService: ApiMessagerService,
   ) {}
 
   exampleFunction(): ServerResponseDto {
@@ -24,31 +26,86 @@ export class RegisterService {
     };
   }
 
-  registerUser(registerDto: RegisterDto): ServerResponseDto {
-    const actualDate = new Date();
-    const salt = BCrypt.genSaltSync(RegisterRules.ENCRYPT_SALT_ROUNDS);
-    const hashedPassword = BCrypt.hashSync(registerDto.password, salt);
+  private async usernameExists(
+    username: string,
+  ): Promise<ServerResponseDto | false> {
+    const result = await this.accountRepository.findOneBy({
+      username: username,
+    });
+    if (result !== null) {
+      return Promise.reject(
+        this.apiMessagerService.createErrorResponse(
+          'This username has already be taken',
+          ResponseStatus.ClientInputError,
+        ),
+      );
+    }
+    return Promise.resolve(false);
+  }
+  private async emailExists(email: string): Promise<ServerResponseDto | false> {
+    const result = await this.accountRepository.findOneBy({
+      email: email,
+    });
+    if (result !== null) {
+      return Promise.reject(
+        this.apiMessagerService.createErrorResponse(
+          'This E-Mail has already be taken',
+          ResponseStatus.ClientInputError,
+        ),
+      );
+    }
+    return Promise.resolve(false);
+  }
+  private async newUser(registerDto: RegisterDto): Promise<ServerResponseDto> {
     try {
-      this.accountRepository.insert({
-        username: registerDto.username,
-        nickname: registerDto.nickname,
-        email: registerDto.email,
-        password: hashedPassword,
-        birthday: actualDate,
-      });
-      return {
+      const actualDate = new Date();
+      const salt = BCrypt.genSaltSync(RegisterRules.ENCRYPT_SALT_ROUNDS);
+      const hashedPassword = BCrypt.hashSync(registerDto.password, salt);
+      await this.accountRepository
+        .insert({
+          username: registerDto.username,
+          nickname: registerDto.nickname,
+          email: registerDto.email,
+          password: hashedPassword,
+          birthday: actualDate,
+        })
+        .catch(() => {
+          return Promise.reject({
+            isError: true,
+            response:
+              'An error occurred on the server, if the error persists, contact us.',
+            responseFrom: 'server',
+            responseStatus: ResponseStatus.InternalServerError,
+          });
+        });
+      return Promise.resolve({
         isError: false,
         response: 'Successfully Registered',
         responseFrom: `server`,
         responseStatus: ResponseStatus.Success,
-      };
+      });
     } catch (error) {
-      return {
-        isError: true,
-        response: 'Failed to register account, please, try again.',
-        responseFrom: 'server',
-        responseStatus: ResponseStatus.InternalServerError,
-      };
+      return Promise.reject(error);
+    }
+  }
+
+  async registerUser(registerDto: RegisterDto): Promise<ServerResponseDto> {
+    try {
+      try {
+        await this.usernameExists(registerDto.username);
+        await this.emailExists(registerDto.email);
+      } catch (error) {
+        return Promise.reject(error);
+      }
+      const registerResult = await this.newUser(registerDto);
+      return Promise.resolve(registerResult);
+    } catch (error) {
+      return Promise.reject(
+        this.apiMessagerService.createErrorResponse(
+          'Failed to register account, please, try again',
+          ResponseStatus.InternalServerError,
+        ),
+      );
     }
   }
 }
