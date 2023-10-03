412ad630a349aca858e55f00858277e9eb108207
LeandroVictor666 <LeandroVictorDev@gmail.com>
2023-10-03 06:39:49 -0300

    feat(controller): updated, optimized and improved RegisterController methods

--- app/api/src/register/register.controller.ts
+++ app/api/src/register/register.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Res, HttpStatus } from '@nestjs/common';
 import { RegisterService } from './register.service';
 import { ServerResponseDto } from 'src/dto/server-response.dto';
 import { RegisterDto } from './register.dto/register.dto';
+import { Response } from 'express';
+import { ResponseStatus } from 'src/enum/response-status.enum';
 
 @Controller('register')
 export class RegisterController {
@@ -12,7 +14,25 @@ export class RegisterController {
     return this.registerService.exampleFunction();
   }
   @Post('register')
-  registerUser(@Body() registerDto: RegisterDto) {
-    return this.registerService.registerUser(registerDto);
+  async registerUser(
+    @Body() registerDto: RegisterDto,
+    @Res({ passthrough: true })
+    res: Response,
+  ) {
+    try {
+      const response: ServerResponseDto =
+        await this.registerService.registerUser(registerDto);
+      res.statusCode = HttpStatus.CREATED;
+      res.send(response);
+    } catch (error) {
+      const responseObject = error as ServerResponseDto;
+      if (responseObject.responseStatus == ResponseStatus.ClientInputError) {
+        res.statusCode = HttpStatus.BAD_REQUEST;
+        res.send(responseObject);
+      } else {
+        res.statusCode = HttpStatus.INTERNAL_SERVER_ERROR;
+        res.send(responseObject);
+      }
+    }
   }
 }
