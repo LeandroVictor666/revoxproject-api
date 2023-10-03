7b93171d164a7518d082d172642c44fb0c83ebd1
LeandroVictor666 <LeandroVictorDev@gmail.com>
2023-10-03 06:52:16 -0300

    feat(module): added ApiMessagerService to RegisterModule providers

--- app/api/src/register/register.module.ts
+++ app/api/src/register/register.module.ts
@@ -3,10 +3,11 @@ import { RegisterController } from './register.controller';
 import { RegisterService } from './register.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Account } from 'src/database/entity/account.entity';
+import { ApiMessagerService } from 'src/api-messager/api-messager.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Account])],
   controllers: [RegisterController],
-  providers: [RegisterService],
+  providers: [RegisterService, ApiMessagerService],
 })
 export class RegisterModule {}
