9ebe795361df59f4f4a4705e15fccb1549704d9a
LeandroVictor666 <LeandroVictorDev@gmail.com>
2023-10-05 06:29:57 -0300

    feat(module): added 'AuthenticationModule' in app.module imports

--- app/api/src/app.module.ts
+++ app/api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { RegisterModule } from './register/register.module';
 import { RevoxdbModule } from './revoxdb/revoxdb.module';
 import { DataSource } from 'typeorm';
+import { AuthenticationModule } from './authentication/authentication.module';
 
 @Module({
-  imports: [RegisterModule, RevoxdbModule],
+  imports: [RegisterModule, RevoxdbModule, AuthenticationModule],
 })
 export class AppModule {
   constructor(private dataSource: DataSource) {}
