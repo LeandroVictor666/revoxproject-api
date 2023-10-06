7757eeabe55534baaac7eadca6a51370ec37e154
LeandroVictor666 <LeandroVictorDev@gmail.com>
2023-10-06 03:13:21 -0300

    feat(module): added 'RedisModule' to app modules imports

--- app/api/src/app.module.ts
+++ app/api/src/app.module.ts
@@ -3,9 +3,19 @@ import { RegisterModule } from './register/register.module';
 import { RevoxdbModule } from './revoxdb/revoxdb.module';
 import { DataSource } from 'typeorm';
 import { AuthenticationModule } from './authentication/authentication.module';
-
+import { RedisModule } from '@songkeys/nestjs-redis';
+import { exec } from 'child_process';
+const redisCommand = 'redis-server';
+exec(redisCommand);
 @Module({
-  imports: [RegisterModule, RevoxdbModule, AuthenticationModule],
+  imports: [
+    RegisterModule,
+    RevoxdbModule,
+    AuthenticationModule,
+    RedisModule.forRoot({
+      config: { host: 'localhost', port: 6379 },
+    }),
+  ],
 })
 export class AppModule {
   constructor(private dataSource: DataSource) {}
