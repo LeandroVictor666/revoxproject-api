71d0a752eb2ef8874d3ecfdd5315dc1ac2fbee69
LeandroVictor666 <LeandroVictorDev@gmail.com>
2023-10-06 03:15:25 -0300

    feat(method): loginUser method from service now require the client IpAddress, updated param passing

--- app/api/src/authentication/authentication.controller.ts
+++ app/api/src/authentication/authentication.controller.ts
@@ -1,14 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Req } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import { LoginDto } from 'src/dto/login-dto';
+import { Request } from 'express';
 
 @Controller('authentication')
 export class AuthenticationController {
   constructor(private readonly authenticationService: AuthenticationService) {}
   @Post('login')
-  async loginUser(@Body() loginDto: LoginDto) {
+  async loginUser(@Body() loginDto: LoginDto, @Req() req: Request) {
     try {
-      const jwtToken = await this.authenticationService.loginUser(loginDto);
+      const jwtToken = await this.authenticationService.loginUser(
+        loginDto,
+        req.ip,
+      );
       return jwtToken;
     } catch (error) {
       return error;
