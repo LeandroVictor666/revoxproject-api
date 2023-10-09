b8bd739b72d137e56a896049a722f3bcef2bb229
LeandroVictor666 <LeandroVictorDev@gmail.com>
2023-10-09 07:53:38 -0300

    feat(interceptor): created publication interceptor (developing)

---
+++ app/api/src/publication/publication.interceptor.spec.ts
@@ -0,0 +1,7 @@
+import { PublicationInterceptor } from './publication.interceptor';
+
+describe('PublicationInterceptor', () => {
+  it('should be defined', () => {
+    expect(new PublicationInterceptor()).toBeDefined();
+  });
+});
