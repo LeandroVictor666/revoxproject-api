f368667f3064fa91c39fc8a17077da3c90acd8eb
LeandroVictor666 <LeandroVictorDev@gmail.com>
2023-10-09 21:40:49 -0300

    feat(route): added 'fire-publication' route/method
    added UseInterceptors, UseGuards and created firePublication method'

--- app/api/src/publication/publication.controller.ts
+++ app/api/src/publication/publication.controller.ts
@@ -1,9 +1,21 @@
-import { Controller, Get, UseGuards, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { PublicationInterceptor } from './publication.interceptor';
 import { AuthenticationGuard } from 'src/authentication/authentication.guard';
+import NewPublicationDto from 'src/dto/new-publication-dto';
+import { PublicationService } from './publication.service';
+import { Request } from 'express';
 
 @Controller('publication')
 export class PublicationController {
+  constructor(private readonly publicationService: PublicationService) {}
   /**
    * Just a test route. ignore.
    * @returns
@@ -15,5 +27,15 @@ export class PublicationController {
     return {
       message: 'hello!',
     };
+  }
+
+  @UseInterceptors(PublicationInterceptor)
+  @UseGuards(AuthenticationGuard)
+  @Post('fire-publication')
+  async firePublication(
+    @Body() bodyRequest: NewPublicationDto,
+    @Req() req: Request,
+  ) {
+    return await this.publicationService.makeNewPublication(bodyRequest, req);
   }
 }
