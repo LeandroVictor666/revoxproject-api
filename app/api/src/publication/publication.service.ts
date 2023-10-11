3bc37acea414dda43ac5c516c9430c900d9fd485
LeandroVictor666 <LeandroVictorDev@gmail.com>
2023-10-11 20:32:25 -0300

    feat(method): updated 'getPublications' and 'getPublicationsQuery' functions, added new method
    Updated/fixed 'getPublications' and getPublicationsQuery method, getPublicationsQuery method now
    uses QueryBuilder to make Postgres Query instead of Repository API, this will fix the problem with
    foreign keys, getPublications now calls 'fixPublicationForeignColumns' method, this method fixes
    Publications Object Properties, read documentation to see more

--- app/api/src/publication/publication.service.ts
+++ app/api/src/publication/publication.service.ts
@@ -7,7 +7,7 @@ import NewPublicationDto from 'src/dto/new-publication-dto';
 import { ServerResponseDto } from 'src/dto/server-response.dto';
 import { ResponseStatus } from 'src/enum/response-status.enum';
 import { ApiMessagerService } from 'src/api-messager/api-messager.service';
-import { MoreThan, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 import PublicationsDto from 'src/dto/publications-dto';
 import PublicationsRequestDto from 'src/dto/publications-request-dto';
 import AccountJwtDto from 'src/dto/account-jwt-dto';
@@ -63,20 +63,71 @@ export class PublicationService {
     });
   }
 
-  private async getPublicationsQuery(lastPublicationId: number) {
-    return await this.publicationsRepository.find({
-      where: { id: MoreThan(lastPublicationId) },
-      order: { id: 'ASC' },
-      take: this.MAX_SELECT_PUBLICATION_QUERY,
+  /**
+   * This function will fix 'Publications' property object, if you don't use it, the object will look like this:
+   *     "publications": [
+        {
+            "id": 57,
+            ...
+            "authorUsername": {
+                "username": "Leandro1337"
+            },
+            "authorId": {
+                "id": 27
+            }
+        },
+    We wants to 'authorUsername' look like this: "authorUsername": "Leandro1337"
+    the same for authorId
+   * @param {Array<Publications>} publications
+   */
+  private fixPublicationForeignColumns(publications: Array<Publications>) {
+    publications.map((publication) => {
+      const authorUsernameFixer = publication.authorUsername as any;
+      const authorIdFixer = publication.authorId as any;
+      publication.authorUsername = authorUsernameFixer.username;
+      publication.authorId = authorIdFixer.id;
     });
   }
-
+  /**
+   * This function will make a query that select all publications with a Limit of 25 (Whatever you want, just change the -
+   * MAX_SELECT_PUBLICATION_QUERY constant) entities
+   * @param {number} lastPublicationId
+   * @returns
+   */
+  private async getPublicationsQuery(lastPublicationId: number) {
+    const result = await this.publicationsRepository
+      .createQueryBuilder('publications')
+      .where('publications.id > :lastPublicationId', { lastPublicationId })
+      .orderBy('publications.id', 'ASC')
+      .take(this.MAX_SELECT_PUBLICATION_QUERY)
+      .leftJoin('publications.authorUsername', 'author')
+      .leftJoin('publications.authorId', 'authorId')
+      .select([
+        'publications.id',
+        'publications.content',
+        'publications.likesCounter',
+        'publications.commentsCounter',
+        'publications.sharesCounter',
+        'publications.publicationDate',
+        'author.username',
+        'authorId.id',
+      ])
+      .getMany();
+    return result;
+  }
+  /**
+   * This function calls the 'getPublicationsQuery' function, which returns a list of publications
+   * Then, it fixes the 'Publication' object properties by calling the 'fixPublicationForeignColumns' function, and then returns the result to the client
+   * @param publicationRequest T
+   * @returns
+   */
   async getPublications(
     publicationRequest: PublicationsRequestDto,
   ): Promise<PublicationsDto> {
     const publications = await this.getPublicationsQuery(
       publicationRequest.lastPublicationId,
     );
+    this.fixPublicationForeignColumns(publications);
     return {
       publications: publications,
       count: publications.length,
